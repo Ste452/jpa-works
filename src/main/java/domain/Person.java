c9883757be9392cb74028c7b8c38d8d9528e6541
Steve <falecomstevevaz@gmail.com>
2024-07-02 18:43:09 -0300

    Find by id

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -2,10 +2,18 @@ package domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String email;
@@ -47,7 +55,4 @@ public class Person implements Serializable {
 	public String toString() {
 		return "Person [id=" + id + ", name=" + name + ", email=" + email + "]";
 	}
-	
-	
-	
 }
